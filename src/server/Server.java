6c0c8e1754a60ece27534747d14733d13cae2de2
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-04-30 10:49:35 +0200

    server implemention

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,15 +1,38 @@
 package server;
 
-import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server extends Thread {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(6578);
+        while (true){
+            try {
+                serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public class ClientWorker implements Runnable{
+        private Socket tagetSocket;
+        private DataInputStream dataInputStream;
+        private DataOutputStream dataOutputStream;
+        public ClientWorker(Socket recieveSocket){
+            try {
+                tagetSocket = recieveSocket;
+                dataInputStream = new DataInputStream(tagetSocket.getInputStream());
+                dataOutputStream = new DataOutputStream(tagetSocket.getOutputStream());
+            }catch (IOException ex){
+              ex.printStackTrace();
+            } }
 
-    private ServerSocket serverSocket;
+        @Override
+        public void run() {
 
-    public static void main(String[] args){
-
+        }
     }
 }
