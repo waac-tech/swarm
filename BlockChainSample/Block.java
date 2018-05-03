960b31d34f38ddb2c8cbc69513e6452f20b9c6cd
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-05-03 11:14:58 +0200

    final block generating push

--- BlockChainSample/Block.java
+++ BlockChainSample/Block.java
@@ -15,5 +15,15 @@ class Block {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
+		this.hash = calculateHash();
+	}
+	
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				data 
+				);
+		return calculatedhash;
 	}
 }
--- BlockChainSample/NoobChain.java
+++ BlockChainSample/NoobChain.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
 class NoobChain {
+	
+		public static ArrayList<Block> blockchain = new ArrayList<Block>(); 
+	
 	public static void main(String[] args) {
+		
+		blockchain.add(new Block("Hi im the first block", "0"));		
+		blockchain.add(new Block("Yo im the secondblock",blockchain.get(blockchain.size()-1).hash)); 
+		blockchain.add(new Block("Hey im the thirdblock",blockchain.get(blockchain.size()-1).hash));
+				
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+		System.out.println(blockchainJson);
+
 		
 	}
 }
